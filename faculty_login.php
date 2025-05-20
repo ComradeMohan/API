1ec13cd732c7d2041073d94b25a448cbea7f36e9
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2025-05-20 11:47:20 +0530

    Update faculty_login.php

--- faculty_login.php
+++ faculty_login.php
@@ -1,17 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "univalut_db";
 
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) {
-    echo json_encode(["success" => false, "message" => "Connection failed"]);
-    exit();
-}
+
+include('db_online.php');
 
 $student_number = $conn->real_escape_string($student_number);
 $password = $conn->real_escape_string($password);
+
 
 $sql = "SELECT * FROM faculty_new WHERE login_id = '$student_number' AND password = '$password' LIMIT 1";
 $result = $conn->query($sql);
