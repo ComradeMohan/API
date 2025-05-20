841c647c7d2a38780b85b59dc6d1c201cc189918
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2025-05-20 14:36:03 +0530

    Update getNextFacultyId.php

--- getNextFacultyId.php
+++ getNextFacultyId.php
@@ -1,18 +1,7 @@
 <?php
 header("Content-Type: application/json");
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "univalut_db";
-
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    echo json_encode(["success" => false, "message" => "Connection failed"]);
-    exit();
-}
-
+include('db_online.php');
 $sql = "SELECT login_id FROM faculty_new ORDER BY id DESC LIMIT 1";
 $result = $conn->query($sql);
 
