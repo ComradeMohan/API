7d166fe290aef9bc6bf4c6dfa2a1a0fd4d2539e4
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2025-05-20 14:31:58 +0530

    Update addCourse.php

--- addCourse.php
+++ addCourse.php
@@ -1,17 +1,7 @@
 <?php
 header("Content-Type: application/json");
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "univalut_db";
-
-$conn = new mysqli($host, $user, $pass, $db);
-
-if ($conn->connect_error) {
-    echo json_encode(["success" => false, "message" => "Connection failed"]);
-    exit();
-}
+include('db_online.php');
 
 $data = json_decode(file_get_contents("php://input"), true);
 
